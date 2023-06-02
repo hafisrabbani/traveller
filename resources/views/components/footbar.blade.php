be57fdfc82a2ac5756005cacd628585c494c235b
mahendrakrs448 <mahendrakrs448@gmail.com>
2023-06-02 09:59:15 +0700

    initial commit

---
+++ resources/views/components/footbar.blade.php
@@ -0,0 +1,31 @@
+    <div class="row">
+        <!-- logo traveller -->
+        <div class="col-md-6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img src="Frame 1.png" alt="logo traveller" />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="ms-4 mt-2">Mudahnya Berwisata Kapan Saja!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- informasi kontak -->
+        <div class="col-md-3">
+            <div class="h4 pb-3">Informasi Kontak</div>
+            <p>srengseng sawah</p>
+            <p>08187365833</p>
+            <p>traveller919@gmail.com</p>
+        </div>
+        <!-- link -->
+        <div class="col-md-1">
+            <div class="h4 pb-3">Link</div>
+            <p>Home</p>
+            <p>About Us</p>
+            <p>Destinasi</p>
+        </div>
+        <!-- follow kami -->
+        <div class="col-md-2"></div>
+    </div>
---
+++ resources/views/template/master.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--========== BOX ICONS ==========-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
+
+    <!--========== CSS ==========-->
+    <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/swal.css') }}">
+    @yield('css')
+
+    <title> @yield('title') | Traveller</title>
+</head>
+
+<body>
+    <!--========== NAVBAR ==========-->
+    @include('components.navbar')
+
+
+    <!--========== CONTENTS ==========-->
+    <div class="my-4 bg-white p-4">
+        @yield('content')
+    </div>
+
+    <!--========== FOOTBAR ==========-->
+    @include('components.footbar')
+
+    <!--========== MAIN JS ==========-->
+    <script src="{{ asset('/assets/js/bootstrap.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
+        crossorigin="anonymous"></script>
+    <script src="{{ asset('/assets/js/swal.js') }}"></script>
+    <script src="https://kit.fontawesome.com/8237a20b1c.js" crossorigin="anonymous"></script>
+
+    <script>
+        @if (session('errors'))
+            Swal.fire({
+                title: 'Pesan!',
+                text: '{{ session()->get('errors')->first() }}',
+                icon: 'warning',
+                showCancelButton: false,
+                showConfirmButton: false,
+                timer: 2000
+            })
+        @endif
+
+        @if (session('success'))
+            Swal.fire({
+                title: 'Pesan!',
+                text: '{{ session()->get('success') }}',
+                icon: 'success',
+                showCancelButton: false,
+                showConfirmButton: false,
+                timer: 2000
+            })
+        @endif
+
+        @if (session('error'))
+            {
+                Swal.fire({
+                    title: 'Pesan!',
+                    text: '{{ session()->get('error') }}',
+                    icon: 'error',
+                    showCancelButton: false,
+                    showConfirmButton: false,
+                    timer: 2000
+                })
+            }
+        @endif
+    </script>
+
+    @yield('js')
+</body>
+
+</html>
